23d5f940fe7bc1d9d6a44eee69220eed79dfb9b6
aleksandar.ninkovic <aleksandar.ninkovic@beamusup.com>
2021-10-25 16:49:19 +0200

    Added compression to .tar.gz

--- db-backup.php
+++ db-backup.php
@@ -28,3 +28,23 @@ if(!empty($output)) {
 } else {
     echo "Database backup DONE\n";
 }
+
+echo "Compressing the backup file to $filename.tar.gz ...\n";
+
+try
+{
+    if (file_exists($filename . '.tar.gz')) {
+        unlink($filename . '.tar.gz');
+    }
+    $archive = new PharData($filename . '.tar');
+    $archive->addFile($filename);
+    $archive->compress(Phar::GZ);
+    unlink($filename . '.tar');
+    unlink($filename);
+}
+catch (Exception $e)
+{
+    echo "Compression operation failed! : " . $e . "\n";
+    die();
+}
+echo "Backup file successfully compressed\n";
